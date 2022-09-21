50dc1a964552932e9b6e23db842d20d1477a6c28
chumardo <dachigiorgadze98@gmail.com>
2022-09-21 12:03:41 +0400

    sum all exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(first_n, second_n) {
+    let sum = 0;
+    for (let i = first_n; i < second_n + 1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
